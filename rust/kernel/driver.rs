b3d1378e36b7854782f924809e89e75da2fcab27
Onur Özkan <work@onurozkan.dev>
2025-06-25 07:30:10 +0300

    rust: simplify `Adapter::id_info`
    It was obviously unnecessary to check if `id` is `Some`.
    
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- rust/kernel/driver.rs
+++ rust/kernel/driver.rs
@@ -178,11 +178,6 @@ pub trait Adapter {
     /// If this returns `None`, it means that there is no match in any of the ID tables directly
     /// associated with a [`device::Device`].
     fn id_info(dev: &device::Device) -> Option<&'static Self::IdInfo> {
-        let id = Self::of_id_info(dev);
-        if id.is_some() {
-            return id;
-        }
-
-        None
+        Self::of_id_info(dev)
     }
 }
