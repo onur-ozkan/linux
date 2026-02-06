cc4b1a34f4677e2fe3db67ffba4e6b4825e6155a
Deborah Brouwer <deborah.brouwer@collabora.com>
2026-02-06 11:37:21 -0800

    fix: gpuvm: fix OpMapped _invariant type

--- rust/kernel/drm/gpuvm/sm_ops.rs
+++ rust/kernel/drm/gpuvm/sm_ops.rs
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0 OR MIT
+#![allow(clippy::type_complexity)]
 
 use super::*;
 
@@ -102,7 +103,7 @@ impl<'op, T: DriverGpuVm> OpMap<'op, T> {
 
 /// Represents a completed [`OpMap`] operation.
 pub struct OpMapped<'op, T> {
-    _invariant: PhantomData<*mut &'op mut T>,
+    _invariant: PhantomData<fn(&'op mut T) -> fn(&'op mut T)>,
 }
 
 /// Represents an `sm_step_unmap` operation that has not yet been completed.
